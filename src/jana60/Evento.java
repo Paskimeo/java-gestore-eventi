62a68fd53010a6b8078321ba5bc890ed4adbf5b3
paskimeo <paskimeo@gmail.com>
2022-07-01 09:37:58 +0200

    creata classe evento

---
+++ src/jana60/Evento.java
@@ -0,0 +1,12 @@
+package jana60;
+
+import java.time.LocalDate;
+
+public class Evento {
+
+	//attributi
+	private String titolo;
+	private LocalDate data;
+	private int postiTotali;
+	private int postiPrenotati = 0;
+}
